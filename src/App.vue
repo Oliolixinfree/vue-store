b2cbdbeaed920f540658594370258b01fa7821d8
Oliolixinfree <vanish8ii@gmail.com>
2025-08-14 15:46:05 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Wrapper from './components/Wrapper.vue'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Wrapper>
+    <Header />
+  </Wrapper>
+</template>
+
+<style scoped></style>
